8d229752132a210fa57063a7757e6fcd7fc212b2
jokeeffeBP <james.okeeffe@bulletproof.net>
2015-05-11 23:53:34 +1000

    Added fourth line and converted to Eclipse Maven project.

--- src/main/java/com/jokeeffe/maven/App.java
+++ src/main/java/com/jokeeffe/maven/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "This is Maven" );
         System.out.println("This is Jenkins");
+        System.out.println("This is Java");
     }
 }
